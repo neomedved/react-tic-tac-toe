3fb2b93165feec06f83c4789e80bafe8473ea653
Artem Medvedev <at.medvedev@gmail.com>
2020-06-26 17:35:36 +0300

    Updates

---
+++ precache-manifest.64611a936967338fbb9d8e6cc15062bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec63f6b75c97cdc34b52651d38550d0f",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "483c071876350f550812",
+    "url": "/react-tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "185550ba4c511e915492",
+    "url": "/react-tic-tac-toe/static/js/2.e977bf79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.e977bf79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "483c071876350f550812",
+    "url": "/react-tic-tac-toe/static/js/main.8285cf8c.chunk.js"
+  },
+  {
+    "revision": "d9255e6de55285e18dc3",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.81654cbd.js"
+  }
+]);
